3d5ffaa6e2a125fb49c7e49acd02c37a7258704f
prinsahmed <s.a.prince.offiziell@gmail.com>
2025-12-15 13:31:35 +0600

    HR apis created

---
+++ index.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = 2500;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.MONGO_DB_SERVER}:${process.env.MONGO_DB_KEY}@cluster0.ufqnoyu.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+
+    try {
+
+        await client.connect();
+        const dataBase = client.db('assetDB');
+        const usersCollection = dataBase.collection('usersCollection')
+        const assetCollection = dataBase.collection('assetCollection')
+
+
+        app.post('/user-employee', async (req, res) => {
+            const EmplyeeData = req.body;
+
+            if (EmplyeeData) {
+                userData.role = 'Employee'
+                const result = await usersCollection.insertOne(EmplyeeData);
+                res.send(result);
+            }
+        })
+
+
+
+        // HR apis
+
+        app.post('/user-HR', async (req, res) => {
+            const HRData = req.body;
+
+            if (HRData) {
+                HRData.role = 'HR'
+                HRData.packageLimit = 5
+                HRData.currentEmployees = 0
+                HRData.subscription = 'basic'
+
+                const result = await usersCollection.insertOne(HRData);
+                res.send(result);
+            }
+        })
+
+        app.post('/add-product', async (req, res) => {
+            const product = req.body;
+            const result = await assetCollection.insertOne(product)
+            res.send(result)
+        })
+
+        app.get('/all-asset', async (req, res) => {
+            const search = req.query.search?.trim();
+            
+
+            let query = {};
+
+            if (search) {
+                query = {
+                    name: { $regex: search }
+                };
+
+            }
+
+            const result = await assetCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/all-asset/:id', async(req, res) => {
+            const {id} = req.params;
+            
+             if (!id || !ObjectId.isValid(id)) {
+                return res.status(400).send({ success: false, message: "Invalid ID" });
+            }
+            const query = {_id: new ObjectId(id)}
+            const result = await assetCollection.findOne(query);
+            res.send(result);
+
+        })
+
+        app.delete('/all-asset/delete/:id', async (req, res) => {
+            const id = req.params.id;
+
+            if (!id || !ObjectId.isValid(id)) {
+                return res.status(400).send({ success: false, message: "Invalid ID" });
+            }
+            const query = { _id: new ObjectId(id) }
+            const result = await assetCollection.deleteOne(query);
+            res.send(result);
+        })
+
+//         app.patch('/all-asset/edit/:id', async (req, res) => {
+//             const { id } = req.params;
+// console.log(id);
+//             if (!id || !ObjectId.isValid(id)) {
+//                 return res.status(400).send({ success: false, message: "Invalid ID" });
+//             }
+
+//             const query = {_id: new ObjectId(id)};
+//             const result = await assetCollection.updateOne(query);
+//             res.send(result);
+//         })
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+})
